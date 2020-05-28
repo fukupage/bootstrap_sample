62d5c010db79baa95b67a43f104d5db4c71af07d
fukupage <4033990+fukupage@users.noreply.github.com>
2020-05-28 10:38:07 +0900

    大まかに完成

--- js/script.js
+++ js/script.js
@@ -1,52 +1,67 @@
 'use strict';
-$(function(){
+$(function () {
   //メニューの開閉
-  $('#menu_show img').click(function(){
+  $('#menu_show img').click(function () {
     $('#nav ul').addClass('active');
     $('#menu_show').addClass('hidden');
     $('#menu_close').removeClass('hidden');
     $('#mask').addClass('masked');
   });
-  $('#menu_close img').click(function(){
+  $('#menu_close img').click(function () {
     $('#nav ul').removeClass('active');
     $('#menu_show').removeClass('hidden');
     $('#menu_close').addClass('hidden');
     $('#mask').removeClass('masked');
   });
   //表示の切り替え
-  $('#home').click(function(){
+  $('#home').click(function () {
     $('#first').removeClass('hidden');
     $('#second').addClass('hidden');
     $('#third').addClass('hidden');
     $('#fourth').addClass('hidden');
     $('#fifth').addClass('hidden');
   });
-  $('#about').click(function(){
+  $('#about').click(function () {
     $('#first').addClass('hidden');
     $('#second').removeClass('hidden');
     $('#third').addClass('hidden');
     $('#fourth').addClass('hidden');
     $('#fifth').addClass('hidden');
   });
-  $('#sample').click(function(){
+  $('#sample').click(function () {
     $('#first').addClass('hidden');
     $('#second').addClass('hidden');
     $('#third').removeClass('hidden');
     $('#fourth').addClass('hidden');
     $('#fifth').addClass('hidden');
   });
-  $('#think').click(function(){
+  $('#think').click(function () {
     $('#first').addClass('hidden');
     $('#second').addClass('hidden');
     $('#third').addClass('hidden');
     $('#fourth').removeClass('hidden');
     $('#fifth').addClass('hidden');
   });
-  $('#form').click(function(){
+  $('#form').click(function () {
     $('#first').addClass('hidden');
     $('#second').addClass('hidden');
     $('#third').addClass('hidden');
     $('#fourth').addClass('hidden');
     $('#fifth').removeClass('hidden');
+  });
+  //LightBox風のイメージ表示（modalimg）
+  $('.modalimg').click(function () {
+    $('#mask').addClass('masked');
+    let src = $(this).attr('data-src');
+    $('#mask').append('<div class="lightbox"></div>');
+    $('.lightbox').append('<img src="" alt="images">');
+    $('.lightbox').children('img').attr('src', src);
+    $('.lightbox').fadeIn('slow');
+    $('.masked').click(function(){
+      if($('.lightbox').length){
+        $('.lightbox').fadeOut('slow', function(){$(this).remove();});
+        $('#mask').removeClass('masked');
+      }
+    });
   });
 });
